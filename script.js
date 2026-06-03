76d45fd9396ef9c84f9c17cfa70e385bfa4c664d
Adeyeye Deborah <adeyeyedebbie14@gmail.com>
2026-06-03 11:29:20 +0100

    made some changes

---
+++ script.js
@@ -0,0 +1,32 @@
+function sub(){
+            let v1 = f1.value;
+            let v2 = f2.value;
+            let r = v1 - v2;
+            result.value = r;
+
+        }
+    
+    
+        function add(){
+            let v1 = Number(f1.value);
+            let v2 = Number(f2.value);
+            let r = v1 + v2;
+            result.value = r;
+
+        }
+    
+        function mul(){
+            let v1 = f1.value;
+            let v2 = f2.value;
+            let r = v1 * v2;
+            result.value = r;
+
+        }
+    
+        function div(){
+            let v1 = f1.value;
+            let v2 = f2.value;
+            let r = v1 / v2;
+            result.value = r;
+
+        }
